2079151dd52dad02bed0fe27397e3d92cde8f04a
YogaDev21 <yogadev2006@gmail.com>
2024-05-31 15:45:07 +0800

    feat: add post creation functionality

--- src/components/shared/LeftSidebar.tsx
+++ src/components/shared/LeftSidebar.tsx
@@ -64,12 +64,12 @@ const LeftSidebar = () => {
           })}
         </ul>
       </div>
-      <Button variant="ghost" className="shad-button_ghost">
-        <img
-          src="/assets/icons/logout.svg"
-          alt="logout"
-          onClick={() => signOut()}
-        />
+      <Button
+        variant="ghost"
+        className="shad-button_ghost"
+        onClick={() => signOut()}
+      >
+        <img src="/assets/icons/logout.svg" alt="logout" />
         <p className="small-medium lg:base-medium">Logout</p>
       </Button>
     </nav>
